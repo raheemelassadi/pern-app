0f079ba6584349d0f88c3c19bf3d09e2008f439c
raheemelassadi1@gmail.com <raheemelassadi1@gmail.com>
2023-10-31 00:32:25 -0700

    implemented delete method

--- server/index.js
+++ server/index.js
@@ -54,12 +54,32 @@ app.get("/todos/:id", async (req, res) => {
 
 app.put("/todos/:id", async (req, res) => {
   try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("todo was updated");
   } catch (err) {
     console.err(err.message);
   }
 });
 
 //delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");

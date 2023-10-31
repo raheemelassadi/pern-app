076723d1f9b5a3a4e639af6458ba9b52d45c5a6d
raheemelassadi1@gmail.com <raheemelassadi1@gmail.com>
2023-10-31 01:42:15 -0700

    created react template, added bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Fragment></Fragment>
+    </div>
+  );
+}
+
+export default App;

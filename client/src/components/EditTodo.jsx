39ca947d46fad2fb9f514cb95849e818f3d7c9ed
raheemelassadi1@gmail.com <raheemelassadi1@gmail.com>
2023-11-03 14:27:11 -0700

    added function to update current todo items

---
+++ client/src/components/EditTodo.jsx
@@ -0,0 +1,81 @@
+import React, { Fragment, useState } from "react";
+
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+
+  const updateDescription = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div class="modal" id={`id${todo.todo_id}`}>
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Todo</h4>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+
+            <div class="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-dismiss="modal"
+                onClick={(e) => updateDescription(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditTodo;
